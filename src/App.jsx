ca6759b666e168db07d316b66195d75435c73be4
yash6234 <villanyash2511@gmail.com>
2026-03-06 15:12:10 +0530

    Update the changes

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,21 @@ import Dashboard from "./Component/Dashboard/Dashboard.jsx";
 import Masters from "./Component/Masters/Masters.jsx";
 import Sales from "./Component/Sales/Sales.jsx";
 import Data from "./Component/Data/Data.jsx";
+import PublicRoute from "./routes/PublicRoute.jsx";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/login" element={<Login />} />
-        <Route path="/dashboard" element={<Layout />}>
+        <Route path="/login" element={<PublicRoute><Login /></PublicRoute>} />
+        <Route
+          path="/dashboard"
+          element={
+
+            <Layout />
+
+          }
+        >
           <Route index element={<Dashboard />} />
           <Route path="masters" element={<Masters />} />
           <Route path="sales" element={<Sales />} />
